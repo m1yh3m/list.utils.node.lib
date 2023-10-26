b09a5420674f0065574f4cd479536626ebd4e336
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2023-10-26 13:34:22 +0530

    update tests

--- spec/lib.spec.js
+++ spec/lib.spec.js
@@ -19,26 +19,23 @@ describe("list.utils.node.lib", () => {
         });
         it("generates accurate values", () => {
             const expected = [ 1, 2, 4, 8, 16 ];
-            const actual = [];
-            for (const curr of lib.select.from(1).take(5).next((i) => (i % 2 === 0 ? 2 * i : i + 1))) {
-                actual.push(curr);
-            }
+            const actual = [ ...lib.select.from(1).take(5).next((i) => (i % 2 === 0 ? 2 * i : i + 1)) ];
             expect(actual).toEqual(expected);
         });
         it("generates numbers from 0 to n", () => {
             const expected = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-            const actual = [];
-            for (const curr of lib.select.from(0).take(10).next((i) => i + 1)) {
-                actual.push(curr);
-            }
+            const actual = [ ...lib.select.from(0).take(10).next((i) => i + 1) ];
+            // for (const curr of lib.select.from(0).take(10).next((i) => i + 1)) {
+            //     actual.push(curr);
+            // }
             expect(actual).toEqual(expected);
         });
         it("generates 2^n", () => {
             const expected = [ 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 ];
-            const actual = [];
-            for (const curr of lib.select.from(2).take(10).next((i) => 2 * i)) {
-                actual.push(curr);
-            }
+            const actual = [ ...lib.select.from(2).take(10).next((i) => 2 * i) ];
+            // for (const curr of lib.select.from(2).take(10).next((i) => 2 * i)) {
+            //     actual.push(curr);
+            // }
             expect(actual).toEqual(expected);
         });
     });
